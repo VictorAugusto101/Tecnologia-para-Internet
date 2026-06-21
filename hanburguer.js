42299546d3c20e344182bfd9f8928b9aba031d77
Victor Augusto <victor102033156261@gmail.com>
2026-06-21 17:21:30 -0300

    11° commit projeto/final

--- hanburguer.js
+++ hanburguer.js
@@ -1,18 +1,24 @@
 const lanches = [
     {
-        nome = "X frango",
-        preco = 22.50 , 
-        imagem = "images/Xfrango.jpg"
+        nome : "X frango",
+        preco : 22.50 , 
+        imagem : "images/Xfrango.jpg"
     },
     {
-        nome = "X tudo",
-        preco = 25.00,
-        imagem = "images/Xtudo.jpg"
+        nome : "X tudo",
+        preco : 25.00,
+        imagem : "images/Xtudo.jpg"
     }
 ];
 
 const listaLanches = document.getElementById("lista-lanches");
 
-    lanches.foreach(lanche => {
-
-}
+    lanches.forEach(lanche => {
+    listaLanches.innerHTML += `
+    <div class="catao-lanche">
+        <img src="${lanche.imagem}" alt="${lanche.nome}">
+        <h2>${lanche.nome}</h2>
+        <p class="preco">${lanche.preco.toFixed(2)}</p>
+    </div>
+    `;
+});
