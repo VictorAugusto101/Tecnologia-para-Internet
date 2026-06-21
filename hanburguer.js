0ddd45a5fb7e1a892535e1b54fcfc92e39a83a4d
Victor Augusto <victor102033156261@gmail.com>
2026-06-21 13:29:25 -0300

    10° commit projeto

---
+++ hanburguer.js
@@ -0,0 +1,18 @@
+const lanches = [
+    {
+        nome = "X frango",
+        preco = 22.50 , 
+        imagem = "images/Xfrango.jpg"
+    },
+    {
+        nome = "X tudo",
+        preco = 25.00,
+        imagem = "images/Xtudo.jpg"
+    }
+];
+
+const listaLanches = document.getElementById("lista-lanches");
+
+    lanches.foreach(lanche => {
+
+}
